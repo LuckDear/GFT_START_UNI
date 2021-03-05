0b157e6f6dcc43e26e292c110d3fd645602fa8eb
LuckDear <78101218+LuckDear@users.noreply.github.com>
2021-03-04 21:26:23 -0300

    Update Vagao.java
    estava testando e acabei mudando, voltei para o código original, sinto muito

--- Exercicio3/src/main/java/Vagao.java
+++ Exercicio3/src/main/java/Vagao.java
@@ -1,15 +1,15 @@
 public class Vagao extends Transporte {
 
     public Vagao(double valor, double peso,int dis) {
-        super(valor, peso,dis);
+        super(valor, peso);
     }
 
     @Override
     public double calculaFrete() {
         if (getPeso() < 15000) {
-            return 5000 + (getPeso() * 0.07) + (getValor() * 0.01) + (getDis() * 0.5) ;
+            return 5000 + (getPeso() * 0.07) + (getValor() * 0.01) ;
         } else {
-            return (getPeso() * 0.07) + (getValor() * 0.01) + (getDis() * 0.5);
+            return (getPeso() * 0.07) + (getValor() * 0.01);
         }
     }
 }
